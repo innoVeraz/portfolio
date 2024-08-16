8b70a32c6fd01f4bcc114d5f4f6c784fb45ad40d
Vera <v.bergstrom83@yahoo.se>
2024-08-16 19:52:41 +0200

    add intro header and about components

---
+++ components/Header.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import { links } from "@/lib/data";
+import { motion } from "framer-motion";
+import Link from "next/link";
+
+import clsx from "clsx";
+
+export default function Header() {
+  return (
+    <header className="z-[999] relative">
+      <motion.div
+        className="fixed top-0  bg-[#f4f7f9] left-1/2  h-[4.5rem] w-full rounded-none border border-white border-opacity-40
+         bg-be bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] 
+         sm:w-[36rem] sm:rounded-full dark:bg-neutral-950 dark:border-black/40 dark:bg-opacity-75"
+        initial={{ y: -100, x: "-50%", opacity: 0 }}
+        animate={{ y: 0, x: "-50%", opacity: 1 }}
+      ></motion.div>
+
+      <nav className="flex fixed top-[0.15rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0">
+        <ul className="flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-neutral-500 sm:w-[initial] sm:flex-nowrap sm:gap-5">
+          {links.map((link) => (
+            <motion.li
+              className="h-3/4 flex items-center justify-center relative"
+              key={link.hash}
+              initial={{ y: -100, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+            >
+              <Link
+                className={clsx(
+                  "flex w-full items-center justify-center p-3 hover:text-neutral-950 transition dark:text-neutral-500 dark:hover:text-neutral-300",
+                  {
+                    "text-gray-950 dark:text-gray-200":
+                      activeSection === link.name,
+                  }
+                )}
+                href={link.hash}
+                onClick={() => setActiveSection(link.name)}
+              >
+                {link.name}
+                {link.name === activeSection && (
+                  <motion.span
+                    className="bg-[#CCD8E2] rounded-full absolute inset-0 -z-10"
+                    layoutId="activeSection"
+                    transition={{
+                      type: "spring",
+                      stiffness: 380,
+                      damping: 30,
+                    }}
+                  ></motion.span>
+                )}
+              </Link>
+            </motion.li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
+}
---
+++ components/Intro.tsx
@@ -0,0 +1,98 @@
+"use client";
+
+import { motion } from "framer-motion";
+
+import Image from "next/image";
+import vera from "@/public/vera.jpg";
+
+import Link from "next/link";
+
+import { HiDownload } from "react-icons/hi";
+import { FaGithubSquare } from "react-icons/fa";
+import { BsArrowRight, BsLinkedin } from "react-icons/bs";
+
+export default function Intro() {
+  return (
+    <section
+      id="home"
+      className="mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]"
+    >
+      <div className="flex items-center justify-center">
+        <div className="relative">
+          <motion.div
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              type: "tween",
+              duration: 0.2,
+            }}
+          >
+            <Image
+              src={vera}
+              alt="profile pic"
+              priority={true}
+              className="h-24 w-24 rounded-full object-cover border-[0.2rem] border-white shadow-xl"
+            />
+          </motion.div>
+        </div>
+      </div>
+
+      <motion.h1
+        className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+      >
+        <span className="font-bold">Hello, my name is Veronica.</span> I&apos;m
+        a <span className="font-bold">Full Stack Developer</span> with{" "}
+        <span className="font-bold">e-commerce</span> experience. I enjoy
+        building <span className="italic">sites & apps</span>. My focus is{" "}
+        <span className="underline">React (Next.js)</span>.
+      </motion.h1>
+      <motion.div
+        className="flex flex-col sm:flex-row items-center justify-center gap-4 px-4 text-lg font-medium"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.1,
+        }}
+      >
+        <Link
+          href="#contact"
+          className="group bg-[#5580A0] text-white text-lg px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-[#344e61] active:scale-105 transition"
+          //   onClick={() => {
+          //     setActiveSection("Contact");
+          //     setTimeOfLastClick(Date.now());
+          //   }}
+        >
+          Contact me!
+        </Link>
+
+        <a
+          className="group text-lg px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
+          href="/cv-veronica.pdf"
+          download
+        >
+          Download CV <HiDownload className="opacity-60 transition" />
+        </a>
+
+        <div className="flex gap-4">
+          <a
+            className=" hover:text-gray-950 text-[24px] flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+            href="https://www.linkedin.com/in/veronica-b-a863b567/"
+            target="_blank"
+          >
+            <BsLinkedin color="#0a66c2" />
+          </a>
+
+          <a
+            className="flex items-center gap-2 rounded-full text-[26px] focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+            href="https://github.com/innoVeraz"
+            target="_blank"
+          >
+            <FaGithubSquare />
+          </a>
+        </div>
+      </motion.div>
+    </section>
+  );
+}
